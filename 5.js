899e90c6de37579df8e4e3dbbda95acd57b7622a
chungclaire <133744637+chungclaire@users.noreply.github.com>
2023-10-18 19:56:01 +0900

    Add files via upload

--- 5.js
+++ 5.js
@@ -1,13 +1,30 @@
-function printhello() {
-    console.log('hello');
+//function
+//-fundamental building block in the program
+//-subprogram can be used multiple times
+//-performs a task or calculates a value
+
+//1. Function declaration
+// function name(param, param2) {body... return; } 
+//one function === one thing
+//naming: doSomething, command, verb 
+//e.g. CreateCardAndPoint → createtard, createPoint
+//function is object in JS
+
+
+function printHello() {
+    console.log('Hello');
 }
-printhello();
+printHello();
 
 function log(message){
     console.log(message);
 }
-log('hello@');
+log('Hello@');
 log(1234);
+
+//2.parameters
+//premitive parameters: passed by value
+//object parameters: passed by reference
 
 function changeName(obj) {
     obj.name = 'coder';
@@ -15,6 +32,25 @@ function changeName(obj) {
 const ellie = { name: 'ellie'};
 changeName(ellie);
 console.log(ellie);
+
+//3.default parameters (added in ES6)
+ function showMessage(message, from){
+    console.log('${message} by ${from}');
+}
+showMessage('Hi!');
+
+//4. Rest parameters (added in ES6)
+function printAll(...args){
+    for (let i =0; i < args, length; i++ ) {
+        console.log(args[i]);
+    }
+    for (const arg of args) {
+        console.log(arg);
+    }
+
+    args.forEach((arg)=> console.log(arg));
+}
+printAll('dream', 'coding', 'ellie');
 
 //5. local scope
 let globalMessage = 'global'; //global variable
@@ -29,3 +65,4 @@ function printMessage() {
     console.log(childMessage)
 }
 printMessage();
+
